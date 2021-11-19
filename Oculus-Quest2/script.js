a2461523998cf0fd545a5bb559d519550bae144f
Manuel Schmalstieg <git.code@ms-studio.net>
2021-11-19 09:27:43 +0000

    etat au vendredi 19 nov

--- Oculus-Quest2/script.js
+++ Oculus-Quest2/script.js
@@ -1,10 +1,12 @@
-// Script qui 
-const container = document.querySelector("container")
-const img = document.querySelector("img")
 
-container.addEventListener("mousemove", (e) => {
-let xAxis = (window.innerWidth / 2 - e.pageX) / 10;
-let yAxis = (window.innerHeight / 2 - e.pageY) / 10;
+const container = document.querySelector("container");
+const img = document.querySelector(".img");
 
-img.style.transform = rotateY(${xAxis}deg) rotateX(${yAxis}deg)
-})
+
+container.addEventListener("mousemove", e => {
+  console.log(e.pageX);
+  let xAxis = (window.innerWidth / 2 - e.pageX) / 10;
+  let yAxis = (window.innerHeight / 2 - e.pageY) / 10;
+  img.style.transform = "translate("+ xAxis +"px,"+ yAxis +"px)";
+  
+});
